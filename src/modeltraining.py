565f52f28cff0951e50d3647a11558b2f0bea6b9
badrinath0526 <badrinathchowdary@gmail.com>
2025-01-16 14:28:12 +0530

    Updated model training

--- src/modeltraining.py
+++ src/modeltraining.py
@@ -1,9 +1,8 @@
 # modeltraining.py
 
 import tensorflow as tf
-from keras._tf_keras.keras.applications import EfficientNetB3
 from keras._tf_keras.keras.models import Sequential
-from keras._tf_keras.keras.layers import Dense, GlobalAveragePooling2D,MaxPooling2D,Conv2D,Dropout,BatchNormalization
+from keras._tf_keras.keras.layers import Dense, GlobalAveragePooling2D,MaxPooling2D,Conv2D,Dropout
 from keras._tf_keras.keras.callbacks import EarlyStopping
 import matplotlib.pyplot as plt
 
@@ -11,16 +10,17 @@ def create_model():
     model = Sequential([
         
         Conv2D(filters=32,kernel_size=(3,3),activation='relu',input_shape=(256,256,3),padding='same'),
-        # BatchNormalization(),
         Conv2D(filters=32,kernel_size=(3,3),activation='relu',padding='same'),
-        # BatchNormalization(),
         MaxPooling2D(pool_size=(2,2)),
         Dropout(0.25),
 
         Conv2D(filters=64,kernel_size=(3,3),activation='relu',padding='same'),
-        # BatchNormalization(),
         Conv2D(filters=64,kernel_size=(3,3),activation='relu',padding='same'),
-        # BatchNormalization(),
+        MaxPooling2D(pool_size=(2,2)),
+        Dropout(0.25),
+
+        Conv2D(filters=128,kernel_size=(3,3),activation='relu',padding='same'),
+        Conv2D(filters=128,kernel_size=(3,3),activation='relu',padding='same'),
         MaxPooling2D(pool_size=(2,2)),
         Dropout(0.25),
         
@@ -30,8 +30,7 @@ def create_model():
         Dense(4, activation='softmax')
     ])
     
-    # for layer in base_model.layers:
-    #     layer.trainable = False  # Freeze the layers of the base model
+
     
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     print(model.summary())
