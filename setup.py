9296f082067ce4afd857202ca02ec7f875bab0c3
badrinath0526 <badrinathchowdary@gmail.com>
2025-01-17 14:06:24 +0530

    Updated setup file

--- setup.py
+++ setup.py
@@ -7,9 +7,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'flask',
-        'pandas',
         'numpy',
-        'scikit-learn',
         'keras',
         'matplotlib',
         'tensorflow',
